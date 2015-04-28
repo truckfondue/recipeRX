770199620ea0eee84a17f536da9903000502ad11
Alex Moore <truckfondue@gmail.com>
2015-04-28 09:45:50 -0600

    minor changes to syntax

--- recipeRX/main.js
+++ recipeRX/main.js
@@ -6,13 +6,7 @@
 // the argument type is not adding to the user experience. This will nee to be added at a later time
 // 
 // Ingredients will be converted or scaled before being added to a recipe
-// var Ingredient = function(name, quantity, unit) {
-// 	this.name = name;
-// 	//this.type = type;
-// 	this.quantity = quantity;
-// 	this.unit = unit;
 
-// }
 
 // Recipe.prototype.convert = function(){
 	
@@ -28,14 +22,14 @@
 
 var Recipe = function(name, ingredients) {
 	this.name = name;
-	this.ingredients = ingredients
-}
+	this.ingredients = ingredients;
+};
 
 //need an add ingrdients prototype to the recipe
 //
 //Recipes will get posted to MyRx
 
-allRecipes is where user created recipes are pushed
+// allRecipes is where user created recipes are pushed
 var allRecipes = [];
 
 $('#create').on('click', function(e){
@@ -44,7 +38,7 @@ $('#create').on('click', function(e){
 	var name = $('#rx-name').val();
 	// helper function to work around name being a problematic naming convention
 	var recipe = name;
-	var items = []
+	var items = [];
 	// using each allows n-number of items to be added to iems
 	$('.clone').each(function(){
 		var name = $(this).find('.name').val();
@@ -55,14 +49,14 @@ $('#create').on('click', function(e){
 			name: name,
 			amount: amount,
 			units: units
-		})
-	})
+		});
+	});
 	console.log(items);
 	console.log(name);
 	// using the Recipe Contructor
 	var recipe = new Recipe(name, items);
 	allRecipes.push(recipe);
-	console.log('created a ingredients list')
+	console.log('created a ingredients list');
 	
 });
 	
@@ -72,21 +66,29 @@ $('#add-items').on('click', function(){
 	var clone = $('#input-template')
   			.clone()
   			.attr('id', null)
-  			.addClass('clone')
+  			.addClass('clone');
 	$('#items').append(clone);
 });
 
 $('.btn-danger').on('click', function(){
-	console.log('Name your Rx event-handler')
+	console.log('Name your Rx event-handler');
 	$('#name-your-rx').attr('id', null);
 
 });
 
 $("#scale-btn").on('click', function(e){
 	e.preventDefault();
-	console.log('Scale Button event-handler')
+	console.log('Scale Button event-handler');
 	var scaleFactor = prompt('What factor would you like to scale by? \n For ex: 2, 3, 1/2...')
 });
+
+var Ingredient = function(name, quantity, unit) {
+	this.name = name;
+	//this.type = type;
+	this.quantity = quantity;
+	this.unit = unit;
+
+};
 
 var chicken = new Ingredient('chicken', 4, 'oz');
 
@@ -94,6 +96,6 @@ var rice = new Ingredient('rice', 4, 'oz');
 
 var cheese = new Ingredient('cheese', 2, 'oz');
 
-var tortilla = new	Ingredient('tortilla', 1, 'ea')
+var tortilla = new	Ingredient('tortilla', 1, 'ea');
 
 var burrito = new Recipe('Burrito', [chicken, rice, cheese, tortilla])
