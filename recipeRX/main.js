e612e8121f5b4e8c2a97dbcf596ffda81a29c1f2
Alex Moore <truckfondue@gmail.com>
2015-05-01 08:09:22 -0600

    midterm presentation

--- recipeRX/main.js
+++ recipeRX/main.js
@@ -2,21 +2,7 @@
   
 // });
 //
-//Initial Ingredient Contructor 
-// the argument type is not adding to the user experience. This will nee to be added at a later time
-// 
-// Ingredients will be converted or scaled before being added to a recipe
 
-
-// Recipe.prototype.convert = function(){
-	
-
-
-// }
-
-// Recipe.prototype.scale = function(recipe){
-
-// }
 
 // ingredients will be an array of individual Ingredient items
 
@@ -30,35 +16,35 @@ var Recipe = function(name, ingredients) {
 //Recipes will get posted to MyRx
 
 // allRecipes is where user created recipes are pushed
-var allRecipes = [];
+// var allRecipes = [];
 
-$('#create').on('click', function(e){
-	e.preventDefault();
-	console.log('creat a recipe test');
-	var name = $('#rx-name').val();
-	// helper function to work around name being a problematic naming convention
-	var recipe = name;
-	var items = [];
-	// using each allows n-number of items to be added to iems
-	$('.clone').each(function(){
-		var name = $(this).find('.name').val();
-		var amount = $(this).find('.amount').val();
-		var units = $(this).find('.units').val();
-		// essentially an Ingredient constructor
-		items.push({
-			name: name,
-			amount: amount,
-			units: units
-		});
-	});
-	console.log(items);
-	console.log(name);
-	// using the Recipe Contructor
-	var recipe = new Recipe(name, items);
-	allRecipes.push(recipe);
-	console.log('created a ingredients list');
+// $('#create').on('click', function(e){
+// 	e.preventDefault();
+// 	console.log('creat a recipe test');
+// 	var name = $('#rx-name').val();
+// 	// helper function to work around name being a problematic naming convention
+// 	var recipe = name;
+// 	var items = [];
+// 	// using each allows n-number of items to be added to iems
+// 	$('.clone').each(function(){
+// 		var name = $(this).find('.name').val();
+// 		var amount = $(this).find('.amount').val();
+// 		var units = $(this).find('.units').val();
+// 		// essentially an Ingredient constructor
+// 		items.push({
+// 			name: name,
+// 			amount: amount,
+// 			units: units
+// 		});
+// 	});
+// 	console.log(items);
+// 	console.log(name);
+// 	// using the Recipe Contructor
+// 	var recipe = new Recipe(name, items);
+// 	allRecipes.push(recipe);
+// 	console.log('created a ingredients list');
 	
-});
+// });
 	
 // allows the user to enter as many ingredients as they wish
 $('#add-items').on('click', function(){
@@ -70,20 +56,38 @@ $('#add-items').on('click', function(){
 	$('#items').append(clone);
 });
 
-$('.btn-danger').on('click', function(){
-	console.log('Name your Rx event-handler');
-	$('#name-your-rx').attr('id', null);
+// $('#name-btn').on('click', function(){
+// 	console.log('Name your Rx event-handler');
+// 	var cloneName = $('#name-your-rx-template')
+// 		.clone()
+// 		.attr('id', null)
+// 		.addClass('rx-dom-element');
+// 	$('#rx-name').append(cloneName);	
+// });
 
+
+$('#name-btn').on('click', function(){
+	console.log('Name your Rx event-handler');
+	$('#name-your-rx-template')
+		.removeClass('hide');
+		
+});
+
+$('#reset').on('click', function(e){
+	e.preventDefault(e);
+	$('.clone').each(function(){
+		$(this).addClass('hide').removeClass('clone');
+		});
+	$('.rx-dom-element').each(function(){
+		$(this).addClass('hide');
+		$('.rx-dom-element').find('input').val('');
+	});
 });
 
 $("#scale-btn").on('click', function(e){
 	e.preventDefault();
 	console.log('Scale Button event-handler');
-<<<<<<< HEAD
-	var scaleFactor = prompt('What factor would you like to scale by? \n For ex: 2, 3, 1/2...')
-});
 
-=======
 	var scaleFactor = prompt('What factor would you like to scale by? \n For ex: 2, 3, 1/2...');
 	$('.clone').each(function(){
 		var amount = $(this).find('.amount').val();
@@ -95,23 +99,24 @@ $("#scale-btn").on('click', function(e){
 $('#convert-btn').on('click', function(e){
 	e.preventDefault(e);
 	alert('By clicking the convert button you have opted to convert your entries to metric.\nUnits will be changed to grams(g) for values less than 1000g. Those larger will be converted to kilograms(kg).\nClick ok to continue.');
+	
 	$('.clone').each(function(){
+		unitsConverted = [];
 		var amount = $(this).find('.amount').val();
 		var units = $(this).find('.units').val();
-		metricConverter(amount, units);
+		unitsConverted.push(metricConverter(amount, units));
 
-		$(this).find('.amount').val(A);
-		$(this).find('.units').val(B);
+
+		$(this).find('.amount').val(unitsConverted[0].amount);
+		$(this).find('.units').val(unitsConverted[0].units);
 		
 	});	
-
+	console.log(unitsConverted);
 });
 
-// var A = [];
-// var B = [];
+
 var metricConverter = function(amount, units) {
-	A = [];
-	B = [];
+
 	var conversionFactor = 1;
 	var metricUnit = 'g';
 	if (units === 'ea') {
@@ -136,14 +141,17 @@ var metricConverter = function(amount, units) {
 		newQuantity = newQuantity/1000;
 		metricUnit = 'kg';
 	}
-	console.log(newQuantity, metricUnit);
-	A.push(newQuantity);
-	B.push(metricUnit);
-	return;
+
+	return {amount: newQuantity, units: metricUnit} ;
 
 };
 
->>>>>>> 86c4b9ea538e69d62ccb1fc233afccff8058490f
+//Initial Ingredient Contructor 
+// the argument type is not adding to the user experience. This will nee to be added at a later time
+// 
+// Ingredients will be converted or scaled before being added to a recipe
+
+
 var Ingredient = function(name, quantity, unit) {
 	this.name = name;
 	//this.type = type;
